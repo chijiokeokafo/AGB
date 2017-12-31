090972305e0d5f07723ea37a3d69e820f5320546
Chijioke Okafo <cokafo@cokafo-mbp.local>
2017-12-31 08:08:45 -0500

    Fixing lawyer phone # left align and nav scroll-to-div on click

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
-  $('.nav-toggle').on('click', function(){
-    $('.nav').toggleClass('show');
+
+  $("ul.nav-container").find("a").click(function(e) {
+      e.preventDefault();
+      var section = $(this).attr("href");
+      $("html, body").animate({
+          scrollTop: $(section).offset().top
+      });
   });
 });
+
+
+
