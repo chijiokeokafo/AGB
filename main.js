be169715fca118577d4ec63491d6b8162fd8acf4
Chijioke Okafo <cokafo@innocean.ca>
2017-12-31 17:56:39 -0500

    updating mobile nav

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 $(document).ready(function(){
 
+  
+
+  $("#mobile-nav-button").click(function(e) {
+      // alert('meee');
+      $(".nav-container").toggleClass("nav-open");
+  });
+
   $("ul.nav-container").find("a").click(function(e) {
       e.preventDefault();
       var section = $(this).attr("href");
       $("html, body").animate({
           scrollTop: $(section).offset().top
       });
+
+      $(".nav-container").css({
+      	"left" : "100%"
+      })
   });
+
 });
 
 
