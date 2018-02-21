c7d9e96f3c902afbb9a67eb0d5bc11aa3602eaaf
Chijioke Okafo <cokafo@innocean.ca>
2018-02-21 10:56:24 -0500

    building about page, and building out skeleton of other pages

--- multi-page/main.js
+++ multi-page/main.js
@@ -1,5 +1,25 @@
 $(document).ready(function(){
 
+  $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:0,
+      nav:false,
+      autoplay:true,
+      autoplayTimeout:5000,
+      autoplayHoverPause:false,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:1
+          },
+          1000:{
+              items:1
+          }
+      }
+  })
+
   
 
   $("#mobile-nav-button").click(function(e) {
