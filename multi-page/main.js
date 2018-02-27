9b5c5e76bfde5af9faaff0abb7c2533a541a8c87
Chijioke Okafo <cokafo@innocean.ca>
2018-02-26 19:15:16 -0500

    fixing notable cases nav to highlight which page it's on

--- multi-page/main.js
+++ multi-page/main.js
@@ -1,24 +1,62 @@
 $(document).ready(function(){
 
   $('.owl-carousel').owlCarousel({
-      loop:true,
-      margin:0,
-      nav:false,
-      autoplay:true,
-      autoplayTimeout:1000,
-      autoplayHoverPause:false,
-      responsive:{
-          0:{
-              items:1
-          },
-          600:{
-              items:1
-          },
-          1000:{
-              items:1
-          }
+    loop:true,
+    margin:0,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:false,
+    responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:1
+      },
+      1000:{
+          items:1
       }
-  })
+    }
+  });
+
+    
+  $("#commercial-btn").click(function(){
+    $(".notable-cases").fadeOut();
+    setTimeout( function () {
+      $("#commercial").fadeIn();
+      $("#regulatory-btn, #public-btn, #tort-btn").css("color", "#144053");
+      $('#commercial-btn').css("color", "#949696");
+    }, 200)
+  });
+
+  $("#regulatory-btn").click(function(){
+    $(".notable-cases").fadeOut();
+    setTimeout( function () {
+      $("#regulatory").fadeIn();
+      $("#commercial-btn, #public-btn, #tort-btn").css("color", "#144053");
+      $('#regulatory-btn').css("color", "#949696");
+    }, 210)
+  });
+
+  $("#public-btn").click(function(){
+    $(".notable-cases").fadeOut();
+    setTimeout( function () {
+      $("#public").fadeIn();
+      $("#commercial-btn, #regulatory-btn, #tort-btn").css("color", "#144053");
+      $('#public-btn').css("color", "#949696");
+    }, 220)
+  });
+
+  $("#tort-btn").click(function(){
+    $(".notable-cases").fadeOut();
+    setTimeout( function () {
+      $("#tort").fadeIn();
+      $("#commercial-btn, #public-btn, #regulatory-btn").css("color", "#144053");
+      $('#tort-btn').css("color", "#949696");
+    }, 230);
+  });
+  
 
   
 
